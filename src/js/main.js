d478bb8010726b31514010b6eefd9a30cd782744
Sebastian Stępień <sebastian.w.stepien@gmail.com>
2021-04-13 17:48:25 +0200

    add prodroduction version to project

---
+++ src/js/main.js
@@ -0,0 +1,90 @@
+import "../sass/style.scss";
+class Doggo {
+  constructor() {
+    this.apiUrl = "https://dog.ceo/api/";
+    this.imgEl = document.querySelector(".featured-dog img");
+    this.backgroundEl = document.querySelector(".featured-dog__background");
+    this.tilesEl = document.querySelector(".tiles");
+    this.spinnerEl = document.querySelector(".spinner");
+
+    this.init();
+  }
+  showLoading() {
+    this.spinnerEl.classList.add("spinner--visible");
+  }
+  hideLoading() {
+    this.spinnerEl.classList.remove("spinner--visible");
+  }
+
+  listBreeds() {
+    return fetch(`${this.apiUrl}breeds/list/all`)
+      .then((resp) => resp.json())
+      .then((data) => data.message);
+  }
+
+  getRandomImage() {
+    return fetch(`${this.apiUrl}breeds/image/random`)
+      .then((resp) => resp.json())
+      .then((data) => data.message);
+  }
+
+  getRandomImageByBreed(breed) {
+    return fetch(`${this.apiUrl}breed/${breed}/images/random`)
+      .then((resp) => resp.json())
+      .then((data) => data.message);
+  }
+  init() {
+    this.showLoading();
+    this.getRandomImage().then((src) => this.showImageWhenReady(src));
+
+    this.showAllBreeds();
+  }
+
+  showImageWhenReady(src) {
+    this.imgEl.setAttribute("src", src);
+    this.backgroundEl.style.backgroundImage = `url("${src}")`;
+    this.hideLoading();
+  }
+
+  addBreed(breed, subBreed) {
+    let name;
+    let type;
+    if (typeof subBreed === "undefined") {
+      name = breed;
+      type = breed;
+    } else {
+      name = `${breed} ${subBreed}`;
+      type = `${breed}/${subBreed}`;
+    }
+    const tile = document.createElement("div");
+    tile.classList.add("tiles__tile");
+    const tileContent = document.createElement("div");
+    tileContent.classList.add("tiles__tile-content");
+    tileContent.innerText = name;
+    tileContent.addEventListener("click", () => {
+      window.scrollTo(0, 0);
+      this.showLoading();
+      this.getRandomImageByBreed(type).then((src) =>
+        this.showImageWhenReady(src)
+      );
+    });
+    tile.appendChild(tileContent);
+    this.tilesEl.appendChild(tile);
+  }
+  showAllBreeds() {
+    this.listBreeds().then((breeds) => {
+      for (const breed in breeds) {
+        if (breeds[breed].length === 0) {
+          this.addBreed(breed);
+        } else {
+          for (const subBreed of breeds[breed]) {
+            this.addBreed(breed, subBreed);
+          }
+        }
+      }
+    });
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  new Doggo();
+});
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/js/main.js",
+  },
+  output: {
+    filename: "js/[name]-bundle-[contenthash:6].js",
+    path: path.resolve(__dirname, "../", "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.sass|scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./[name]-bundle-[contenthash:6].css",
+    }),
+  ],
+};
